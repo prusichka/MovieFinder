300e05da4da8ffd36375ee84597a2350a0e33030
prusichka <denchuk.prusak@gmail.com>
2023-10-19 13:44:59 +0300

    add vercel config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,27 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
   pages: true,
+  app: {
+		head: {
+			meta: [
+				{ name: 'description', content: 'Movie Finder' },
+				{ name: 'name', content: 'Movie Finder' },
+			],
+			htmlAttrs: {
+				lang: 'en',
+			},
+		},
+	},
 	runtimeConfig: {
     public: {
       apiBase: 'https://www.omdbapi.com/',
 			apiKey: '47d81a02'
     }
   },
+  nitro: {
+		compressPublicAssets: true,
+		preset: 'vercel-edge',
+	},
   css: ['~/assets/css/main.css'],
 	modules: ['@pinia/nuxt','@nuxtjs/tailwindcss','@element-plus/nuxt'],
 })
